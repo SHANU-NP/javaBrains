a43c7ceb78b2a666ec68499cf1bd76155f23354a
shanuNP <shanunp2016@gmail.com>
2023-04-03 18:12:20 +0530

    initial commit

---
+++ src/main/java/com/javaBrains/javaBrains/JavaBrainsApplication.java
@@ -0,0 +1,13 @@
+package com.javaBrains.javaBrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaBrains/javaBrains/JavaBrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaBrains.javaBrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
