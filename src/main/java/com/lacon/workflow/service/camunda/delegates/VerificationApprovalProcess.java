e1692f0ca5274aa73a55ebd2233a908a3734c5e8
shanuNP <shanunp2016@gmail.com>
2024-05-18 15:41:39 +0530

    feat: delete implementation updated

--- src/main/java/com/lacon/workflow/service/camunda/delegates/VerificationApprovalProcess.java
+++ src/main/java/com/lacon/workflow/service/camunda/delegates/VerificationApprovalProcess.java
@@ -1,16 +1,17 @@
 package com.lacon.workflow.service.camunda.delegates;
 
 import com.lacon.workflow.service.camunda.eventListeners.SupervisorListener;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VerificationApprovalProcess implements TaskListener {
+public class VerificationApprovalProcess implements JavaDelegate {
     @Override
-    public void notify(DelegateTask delegateTask) {
+    public void execute(DelegateExecution delegateExecution) throws Exception {
         final Logger log = LoggerFactory.getLogger(VerificationApprovalProcess.class);
         log.info("enter into VerificationApprovalProcess class");
-
     }
 }
--- src/main/java/com/lacon/workflow/service/camunda/delegates/VerificationRejectProcess.java
+++ src/main/java/com/lacon/workflow/service/camunda/delegates/VerificationRejectProcess.java
@@ -1,13 +1,16 @@
 package com.lacon.workflow.service.camunda.delegates;
 
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.delegate.TaskListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VerificationRejectProcess implements TaskListener {
+public class VerificationRejectProcess implements JavaDelegate {
+
     @Override
-    public void notify(DelegateTask delegateTask) {
+    public void execute(DelegateExecution delegateExecution) throws Exception {
         final Logger log = LoggerFactory.getLogger(VerificationRejectProcess.class);
         log.info("enter into VerificationRejectProcess class");
     }
